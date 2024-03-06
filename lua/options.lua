d84b5dde802a28c7a0367ee18d492f0f757be41c
Sergei Tian <s.tian.spb@gmail.com>
2024-03-06 22:15:10 +0300

    set nohlsearch

--- lua/options.lua
+++ lua/options.lua
@@ -11,19 +11,21 @@ opt.rnu = true
 cmd[[
 filetype indent on
 syntax enable
+set nohlsearch
 ]]
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.smartindent = true
 
+
 cmd [[
 autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 ]]
 
-cmd'colorscheme onedark'
+-- cmd'colorscheme onedark'
 -- cmd'colorscheme melange'
--- cmd'colorscheme nordic'
+cmd'colorscheme nordic'
 -- cmd'colorscheme gruvbox'
 -- cmd'colorscheme tokyonight'
 -- cmd'colorscheme dracula'
